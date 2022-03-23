81d3ec8826fd42fed76853a00d5a2c738e43de6e
noko1024 <ynoko1024@gmail.com>
2022-03-23 18:36:43 +0900

    watchdogでファイル更新を監視して更新するように 未テストだけど

--- test.py
+++ test.py
@@ -2,9 +2,26 @@
 
 import discord
 import json
+from  watchdog.events import PatternMatchingEventHandler
+from watchdog.observers import Observer
 
 token = json.load(open('./option.json','r'))["token"]
 client = discord.Client()
+reply_db = None
+
+
+def on_modified(__):
+    global reply_db
+    with open('./bot_db.json') as f:
+        reply_db = json.load(f)
+
+event_handler = PatternMatchingEventHandler()
+event_handler.on_modified = on_modified
+    
+observer = Observer()
+observer.schedule(event_handler, './bot_db.json', recursive=True)
+observer.start()
+
 
 @client.event
 async def on_ready():
@@ -18,8 +35,6 @@ async def on_message(message):
 
     if message.content.startswith(prefix):
         receive_ms = message.content
-        with open('./bot_db.json') as f:
-            reply_db = json.load(f)
         reply = reply_db.get(receive_ms[len(prefix):])
         if reply is None:
             reply = "未登録" #もしdbにkeyが存在しなかったときに返す言葉
